8ee007c3299a33ee814b0f61b453a1f8a0c355c0
Haydn Ewers <27211+haydn@users.noreply.github.com>
2021-01-16 12:09:55 +1100

    Add basic create and update functions

--- index.ts
+++ index.ts
@@ -1,9 +1,6 @@
 type Graph = {
   size: number;
-  adjacencyMatrix: Array<Array<number>>;
-  // edgeWeight: Array<Array<Array<number>>>;
-  // vertexData: Array<V>;
-  // edgeData: Array<Array<E>>;
+  adjacencyMatrix: { [u: string]: { [v: string]: number } };
 };
 
 type D3Graph = {
@@ -16,67 +13,122 @@ type D3Graph = {
   }>;
 };
 
-const toD3 = (graph: Graph): D3Graph => {
-  const nodes = [];
-  for (let i = 0; i < graph.size; i++) {
-    nodes[i] = { id: i.toString(10) };
-  }
+const addEdge = (graph: Graph, edge: [string, string]): Graph => {
+  const size = graph.size;
+  const adjacencyMatrix: Graph["adjacencyMatrix"] = {};
 
-  const links = [];
-  for (let u = 0; u < graph.size; u++) {
-    for (let v = 0; v < graph.size; v++) {
-      const edges = graph.adjacencyMatrix[u][v];
-      if (edges !== undefined) {
-        for (let i = 0; i < edges; i++) {
-          links.push({
-            source: u.toString(10),
-            target: v.toString(10),
-          });
-        }
-      }
+  for (let u in graph.adjacencyMatrix) {
+    adjacencyMatrix[u] = {};
+    for (let v in graph.adjacencyMatrix[u]) {
+      adjacencyMatrix[u][v] = graph.adjacencyMatrix[u][v];
     }
   }
 
-  return { nodes, links };
-};
-
-const fromD3 = (graph: D3Graph): Graph => {
-  const idToIndexMap: {
-    [id: string]: number;
-  } = {};
-  for (let i = 0; i < graph.nodes.length; i++) {
-    idToIndexMap[graph.nodes[i].id] = i;
-  }
-
-  const adjacencyMatrix: Graph["adjacencyMatrix"] = [];
-  for (let u = 0; u < graph.nodes.length; u++) {
-    adjacencyMatrix[u] = [];
-    for (let v = 0; v < graph.nodes.length; v++) {
-      adjacencyMatrix[u][v] = 0;
-    }
-  }
-  for (let link of graph.links) {
-    const u = idToIndexMap[link.source];
-    const v = idToIndexMap[link.target];
-    adjacencyMatrix[u][v] += 1;
-  }
+  adjacencyMatrix[edge[0]][edge[1]] += 1;
 
   return {
-    size: graph.nodes.length,
+    size,
     adjacencyMatrix,
   };
 };
 
+const addVertex = (graph: Graph, vertex: string): Graph => {
+  if (graph.adjacencyMatrix[vertex]) return graph;
+
+  const size = graph.size + 1;
+  const adjacencyMatrix: Graph["adjacencyMatrix"] = {};
+
+  for (let u in graph.adjacencyMatrix) {
+    adjacencyMatrix[u] = {};
+    for (let v in graph.adjacencyMatrix[u]) {
+      adjacencyMatrix[u][v] = graph.adjacencyMatrix[u][v];
+    }
+  }
+
+  for (let v in adjacencyMatrix) adjacencyMatrix[v][vertex] = 0;
+  adjacencyMatrix[vertex] = {};
+  for (let v in adjacencyMatrix) adjacencyMatrix[vertex][v] = 0;
+
+  return {
+    size,
+    adjacencyMatrix,
+  };
+};
+
+const create = (
+  size: number = 0,
+  id: (i: number) => string = (i) => i.toString(10)
+): Graph => {
+  const adjacencyMatrix: Graph["adjacencyMatrix"] = {};
+
+  for (let i = 0; i < size; i++) {
+    const u = id(i);
+    adjacencyMatrix[u] = {};
+    for (let j = 0; j < size; j++) {
+      const v = id(j);
+      adjacencyMatrix[u][v] = 0;
+    }
+  }
+
+  return {
+    size,
+    adjacencyMatrix,
+  };
+};
+
+const fromD3 = (d3Graph: D3Graph): Graph => {
+  const size = d3Graph.nodes.length;
+  const adjacencyMatrix: Graph["adjacencyMatrix"] = {};
+
+  for (let u of d3Graph.nodes) {
+    adjacencyMatrix[u.id] = {};
+    for (let v of d3Graph.nodes) {
+      adjacencyMatrix[u.id][v.id] = 0;
+    }
+  }
+
+  for (let link of d3Graph.links) {
+    adjacencyMatrix[link.source][link.target] += 1;
+  }
+
+  return {
+    size,
+    adjacencyMatrix,
+  };
+};
+
+const inDegrees = (graph: Graph): { [id: string]: number } => {
+  const result: { [id: string]: number } = {};
+  for (let i in graph.adjacencyMatrix) result[i] = 0;
+  for (let u in graph.adjacencyMatrix) {
+    for (let v in graph.adjacencyMatrix[u]) {
+      result[v] += graph.adjacencyMatrix[u][v];
+    }
+  }
+  return result;
+};
+
+const isCyclic = (graph: Graph): boolean => {
+  const visited: Set<string> = new Set();
+  const path: Set<string> = new Set();
+  for (let i in graph.adjacencyMatrix) {
+    if (visited.has(i)) continue;
+    const cycleFound = _isCyclic(graph, visited, path, i);
+    if (cycleFound) return true;
+  }
+  return false;
+};
+
 const _isCyclic = (
   graph: Graph,
-  visited: Set<number>,
-  path: Set<number>,
-  vertex: number
+  visited: Set<string>,
+  path: Set<string>,
+  vertex: string
 ): boolean => {
   visited.add(vertex);
   path.add(vertex);
 
-  for (let i = 0; i < graph.size; i++) {
+  for (let i in graph.adjacencyMatrix[vertex]) {
     if (graph.adjacencyMatrix[vertex][i] > 0) {
       if (visited.has(i)) {
         if (path.has(i)) return true;
@@ -92,49 +144,92 @@ const _isCyclic = (
   return false;
 };
 
-const isCyclic = (graph: Graph): boolean => {
-  const visited: Set<number> = new Set();
-  const path: Set<number> = new Set();
-  for (let i = 0; i < graph.size; i++) {
-    if (visited.has(i)) continue;
-    const cycleFound = _isCyclic(graph, visited, path, i);
-    if (cycleFound) return true;
-  }
-  return false;
-};
-
-const getInDegrees = (graph: Graph): Array<number> => {
-  const result: Array<number> = [];
-  for (let i = 0; i < graph.size; i++) {
-    result[i] = 0;
-  }
-  for (let u = 0; u < graph.size; u++) {
-    for (let v = 0; v < graph.size; v++) {
-      result[v] += graph.adjacencyMatrix[u][v];
+const outDegrees = (graph: Graph): { [id: string]: number } => {
+  const result: { [id: string]: number } = {};
+  for (let i in graph.adjacencyMatrix) result[i] = 0;
+  for (let u in graph.adjacencyMatrix) {
+    for (let v in graph.adjacencyMatrix[u]) {
+      result[v] += graph.adjacencyMatrix[v][u];
     }
   }
   return result;
 };
 
-const topologicalSort = (graph: Graph): Array<number> => {
+const removeEdge = (graph: Graph, edge: [string, string]): Graph => {
+  const size = graph.size;
+  const adjacencyMatrix: Graph["adjacencyMatrix"] = {};
+
+  for (let u in graph.adjacencyMatrix) {
+    adjacencyMatrix[u] = {};
+    for (let v in graph.adjacencyMatrix[u]) {
+      adjacencyMatrix[u][v] = graph.adjacencyMatrix[u][v];
+    }
+  }
+
+  if (adjacencyMatrix[edge[0]][edge[1]] > 0) {
+    adjacencyMatrix[edge[0]][edge[1]] -= 1;
+  }
+
+  return {
+    size,
+    adjacencyMatrix,
+  };
+};
+
+const removeVertex = (graph: Graph, vertex: string): Graph => {
+  const size = graph.size - 1;
+  const adjacencyMatrix: Graph["adjacencyMatrix"] = {};
+
+  for (let u in graph.adjacencyMatrix) {
+    if (u === vertex) continue;
+    adjacencyMatrix[u] = {};
+    for (let v in graph.adjacencyMatrix[u]) {
+      if (v === vertex) continue;
+      adjacencyMatrix[u][v] = graph.adjacencyMatrix[u][v];
+    }
+  }
+
+  return {
+    size,
+    adjacencyMatrix,
+  };
+};
+
+const toD3 = (graph: Graph): D3Graph => {
+  const nodes = [];
+  const links = [];
+
+  for (let u in graph.adjacencyMatrix) {
+    nodes[nodes.length] = { id: u };
+    for (let v in graph.adjacencyMatrix[u]) {
+      for (let i = 0; i < graph.adjacencyMatrix[u][v]; i++) {
+        links[links.length] = { source: u, target: v };
+      }
+    }
+  }
+
+  return { nodes, links };
+};
+
+const topologicalSort = (graph: Graph): Array<string> => {
   if (isCyclic(graph)) throw "Cannot sort a graph that contains cycles.";
 
-  const result: Array<number> = [];
-  const visited: Set<number> = new Set();
-  const queue: Array<number> = [];
-  const inDegree = getInDegrees(graph);
-  
-  for (let i = 0; i < graph.size; i++) {
+  const result: Array<string> = [];
+  const visited: Set<string> = new Set();
+  const queue: Array<string> = [];
+  const inDegree = inDegrees(graph);
+
+  for (let i in graph.adjacencyMatrix) {
     if (inDegree[i] === 0) {
       queue.push(i);
       visited.add(i);
     }
   }
-  
+
   while (queue.length > 0) {
     const v = queue.shift();
     result.push(v);
-    for (let i = 0; i < graph.size; i++) {
+    for (let i in graph.adjacencyMatrix) {
       if (graph.adjacencyMatrix[v][i] > 0 && !visited.has(i)) {
         inDegree[i] -= graph.adjacencyMatrix[v][i];
         if (inDegree[i] <= 0) {
@@ -148,4 +243,18 @@ const topologicalSort = (graph: Graph): Array<number> => {
   return result;
 };
 
-export { Graph, D3Graph, isCyclic, topologicalSort, fromD3, toD3 };
+export {
+  D3Graph,
+  Graph,
+  addEdge,
+  addVertex,
+  create,
+  fromD3,
+  inDegrees,
+  isCyclic,
+  outDegrees,
+  removeEdge,
+  removeVertex,
+  toD3,
+  topologicalSort,
+};
--- test.ts
+++ test.ts
@@ -1,190 +1,384 @@
-import { fromD3, isCyclic, toD3, topologicalSort } from "./index";
+import {
+  addEdge,
+  addVertex,
+  create,
+  fromD3,
+  inDegrees,
+  isCyclic,
+  outDegrees,
+  removeEdge,
+  removeVertex,
+  toD3,
+  topologicalSort,
+} from "./index";
 
-import { strict as assert } from "assert";
+import test from "tape";
 
-assert.equal(
-  isCyclic({
-    size: 1,
-    adjacencyMatrix: [[0]],
-  }),
-  false
-);
+test("addEdge", (t) => {
+  t.plan(1);
 
-assert.equal(
-  isCyclic({
-    size: 2,
-    adjacencyMatrix: [
-      [0, 0],
-      [0, 0],
-    ],
-  }),
-  false
-);
-
-assert.equal(
-  isCyclic({
-    size: 2,
-    adjacencyMatrix: [
-      [0, 1],
-      [0, 0],
-    ],
-  }),
-  false
-);
-
-assert.equal(
-  isCyclic({
-    size: 3,
-    adjacencyMatrix: [
-      [0, 1, 1],
-      [0, 0, 0],
-      [0, 0, 0],
-    ],
-  }),
-  false
-);
-
-assert.equal(
-  isCyclic({
-    size: 1,
-    adjacencyMatrix: [[1]],
-  }),
-  true
-);
-
-assert.equal(
-  isCyclic({
-    size: 2,
-    adjacencyMatrix: [
-      [0, 1],
-      [1, 0],
-    ],
-  }),
-  true
-);
-
-assert.equal(
-  isCyclic({
-    size: 3,
-    adjacencyMatrix: [
-      [0, 1, 0],
-      [0, 0, 1],
-      [1, 0, 0],
-    ],
-  }),
-  true
-);
-
-assert.equal(
-  isCyclic({
-    size: 2,
-    adjacencyMatrix: [
-      [1, 0],
-      [0, 1],
-    ],
-  }),
-  true
-);
-
-assert.deepEqual(
-  topologicalSort({
-    size: 1,
-    adjacencyMatrix: [[0]],
-  }),
-  [0]
-);
-
-assert.deepEqual(
-  topologicalSort({
-    size: 3,
-    adjacencyMatrix: [
-      [0, 0, 0],
-      [0, 0, 0],
-      [0, 0, 0],
-    ],
-  }),
-  [0, 1, 2]
-);
-
-assert.deepEqual(
-  topologicalSort({
-    size: 3,
-    adjacencyMatrix: [
-      [0, 1, 0],
-      [0, 0, 0],
-      [1, 0, 0],
-    ],
-  }),
-  [2, 0, 1]
-);
-
-assert.deepEqual(
-  topologicalSort({
-    size: 3,
-    adjacencyMatrix: [
-      [0, 0, 0],
-      [1, 0, 0],
-      [1, 0, 0],
-    ],
-  }),
-  [1, 2, 0]
-);
-
-assert.deepEqual(
-  topologicalSort({
-    size: 2,
-    adjacencyMatrix: [
-      [0, 0],
-      [10, 0],
-    ],
-  }),
-  [1, 0]
-);
-
-assert.throws(() => {
-  topologicalSort({
-    size: 2,
-    adjacencyMatrix: [
-      [0, 1],
-      [1, 0],
-    ],
-  });
+  t.deepEqual(
+    addEdge(
+      {
+        size: 2,
+        adjacencyMatrix: {
+          a: { a: 0, b: 0 },
+          b: { a: 0, b: 0 },
+        },
+      },
+      ["a", "b"]
+    ),
+    {
+      size: 2,
+      adjacencyMatrix: {
+        a: { a: 0, b: 1 },
+        b: { a: 0, b: 0 },
+      },
+    }
+  );
 });
 
-assert.deepEqual(
-  fromD3({
-    nodes: [{ id: "a" }, { id: "b" }, { id: "c" }],
-    links: [
-      { source: "a", target: "b" },
-      { source: "a", target: "c" },
-      { source: "a", target: "a" },
-    ],
-  }),
-  {
-    size: 3,
-    adjacencyMatrix: [
-      [1, 1, 1],
-      [0, 0, 0],
-      [0, 0, 0],
-    ],
-  }
-);
+test("addVertex", (t) => {
+  t.plan(2);
 
-assert.deepEqual(
-  toD3({
-    size: 3,
-    adjacencyMatrix: [
-      [1, 1, 1],
-      [0, 0, 0],
-      [0, 0, 0],
-    ],
-  }),
-  {
-    nodes: [{ id: "0" }, { id: "1" }, { id: "2" }],
-    links: [
-      { source: "0", target: "0" },
-      { source: "0", target: "1" },
-      { source: "0", target: "2" },
-    ],
-  }
-);
+  t.deepEqual(
+    addVertex(
+      {
+        size: 0,
+        adjacencyMatrix: {},
+      },
+      "a"
+    ),
+    {
+      size: 1,
+      adjacencyMatrix: {
+        a: { a: 0 },
+      },
+    }
+  );
+
+  t.deepEqual(
+    addVertex(
+      {
+        size: 1,
+        adjacencyMatrix: {
+          a: { a: 0 },
+        },
+      },
+      "b"
+    ),
+    {
+      size: 2,
+      adjacencyMatrix: {
+        a: { a: 0, b: 0 },
+        b: { a: 0, b: 0 },
+      },
+    }
+  );
+});
+
+test("create", (t) => {
+  t.plan(1);
+
+  t.deepEqual(
+    create(2, (i) => `${i}!`),
+    {
+      size: 2,
+      adjacencyMatrix: {
+        "0!": { "0!": 0, "1!": 0 },
+        "1!": { "0!": 0, "1!": 0 },
+      },
+    }
+  );
+});
+
+test("fromD3", (t) => {
+  t.plan(1);
+
+  t.deepEqual(
+    fromD3({
+      nodes: [{ id: "a" }, { id: "b" }, { id: "c" }],
+      links: [
+        { source: "a", target: "b" },
+        { source: "a", target: "c" },
+        { source: "a", target: "a" },
+      ],
+    }),
+    {
+      size: 3,
+      adjacencyMatrix: {
+        a: { a: 1, b: 1, c: 1 },
+        b: { a: 0, b: 0, c: 0 },
+        c: { a: 0, b: 0, c: 0 },
+      },
+    }
+  );
+});
+
+test("inDegrees", (t) => {
+  t.plan(1);
+
+  t.deepEqual(
+    inDegrees({
+      size: 3,
+      adjacencyMatrix: {
+        a: { a: 1, b: 1, c: 1 },
+        b: { a: 0, b: 1, c: 0 },
+        c: { a: 0, b: 0, c: 0 },
+      },
+    }),
+    { a: 1, b: 2, c: 1 }
+  );
+});
+
+test("isCyclic", (t) => {
+  t.plan(8);
+
+  t.equal(
+    isCyclic({
+      size: 1,
+      adjacencyMatrix: {
+        a: { a: 0 },
+      },
+    }),
+    false
+  );
+
+  t.equal(
+    isCyclic({
+      size: 2,
+      adjacencyMatrix: {
+        a: { a: 0, b: 0 },
+        b: { a: 0, b: 0 },
+      },
+    }),
+    false
+  );
+
+  t.equal(
+    isCyclic({
+      size: 2,
+      adjacencyMatrix: {
+        a: { a: 0, b: 1 },
+        b: { a: 0, b: 0 },
+      },
+    }),
+    false
+  );
+
+  t.equal(
+    isCyclic({
+      size: 3,
+      adjacencyMatrix: {
+        a: { a: 0, b: 1, c: 1 },
+        b: { a: 0, b: 0, c: 0 },
+        c: { a: 0, b: 0, c: 0 },
+      },
+    }),
+    false
+  );
+
+  t.equal(
+    isCyclic({
+      size: 1,
+      adjacencyMatrix: {
+        a: { a: 1 },
+      },
+    }),
+    true
+  );
+
+  t.equal(
+    isCyclic({
+      size: 2,
+      adjacencyMatrix: {
+        a: { a: 0, b: 1 },
+        b: { a: 1, b: 0 },
+      },
+    }),
+    true
+  );
+
+  t.equal(
+    isCyclic({
+      size: 3,
+      adjacencyMatrix: {
+        a: { a: 0, b: 1, c: 0 },
+        b: { a: 0, b: 0, c: 1 },
+        c: { a: 1, b: 0, c: 0 },
+      },
+    }),
+    true
+  );
+
+  t.equal(
+    isCyclic({
+      size: 2,
+      adjacencyMatrix: {
+        a: { a: 1, b: 0 },
+        b: { a: 0, b: 1 },
+      },
+    }),
+    true
+  );
+});
+
+test("outDegrees", (t) => {
+  t.plan(1);
+
+  t.deepEqual(
+    outDegrees({
+      size: 3,
+      adjacencyMatrix: {
+        a: { a: 1, b: 1, c: 1 },
+        b: { a: 0, b: 1, c: 0 },
+        c: { a: 0, b: 0, c: 0 },
+      },
+    }),
+    { a: 3, b: 1, c: 0 }
+  );
+});
+
+test("removeEdge", (t) => {
+  t.plan(1);
+
+  t.deepEqual(
+    removeEdge(
+      {
+        size: 3,
+        adjacencyMatrix: {
+          a: { a: 0, b: 1, c: 0 },
+          b: { a: 0, b: 0, c: 1 },
+          c: { a: 1, b: 0, c: 0 },
+        },
+      },
+      ["a", "b"]
+    ),
+    {
+      size: 3,
+      adjacencyMatrix: {
+        a: { a: 0, b: 0, c: 0 },
+        b: { a: 0, b: 0, c: 1 },
+        c: { a: 1, b: 0, c: 0 },
+      },
+    }
+  );
+});
+
+test("removeVertex", (t) => {
+  t.plan(1);
+
+  t.deepEqual(
+    removeVertex(
+      {
+        size: 3,
+        adjacencyMatrix: {
+          a: { a: 0, b: 1, c: 0 },
+          b: { a: 0, b: 0, c: 1 },
+          c: { a: 1, b: 0, c: 0 },
+        },
+      },
+      "b"
+    ),
+    {
+      size: 2,
+      adjacencyMatrix: {
+        a: { a: 0, c: 0 },
+        c: { a: 1, c: 0 },
+      },
+    }
+  );
+});
+
+test("toD3", (t) => {
+  t.plan(1);
+  t.deepEqual(
+    toD3({
+      size: 3,
+      adjacencyMatrix: {
+        a: { a: 1, b: 1, c: 1 },
+        b: { a: 0, b: 0, c: 0 },
+        c: { a: 0, b: 0, c: 0 },
+      },
+    }),
+    {
+      nodes: [{ id: "a" }, { id: "b" }, { id: "c" }],
+      links: [
+        { source: "a", target: "a" },
+        { source: "a", target: "b" },
+        { source: "a", target: "c" },
+      ],
+    }
+  );
+});
+
+test("topologicalSort", (t) => {
+  t.plan(6);
+
+  t.deepEqual(
+    topologicalSort({
+      size: 1,
+      adjacencyMatrix: {
+        a: { a: 0 },
+      },
+    }),
+    ["a"]
+  );
+
+  t.deepEqual(
+    topologicalSort({
+      size: 3,
+      adjacencyMatrix: {
+        a: { a: 0, b: 0, c: 0 },
+        b: { a: 0, b: 0, c: 0 },
+        c: { a: 0, b: 0, c: 0 },
+      },
+    }),
+    ["a", "b", "c"]
+  );
+
+  t.deepEqual(
+    topologicalSort({
+      size: 3,
+      adjacencyMatrix: {
+        a: { a: 0, b: 1, c: 0 },
+        b: { a: 0, b: 0, c: 0 },
+        c: { a: 1, b: 0, c: 0 },
+      },
+    }),
+    ["c", "a", "b"]
+  );
+
+  t.deepEqual(
+    topologicalSort({
+      size: 3,
+      adjacencyMatrix: {
+        a: { a: 0, b: 0, c: 0 },
+        b: { a: 1, b: 0, c: 0 },
+        c: { a: 1, b: 0, c: 0 },
+      },
+    }),
+    ["b", "c", "a"]
+  );
+
+  t.deepEqual(
+    topologicalSort({
+      size: 2,
+      adjacencyMatrix: {
+        a: { a: 0, b: 0 },
+        b: { a: 10, b: 0 },
+      },
+    }),
+    ["b", "a"]
+  );
+
+  t.throws(() => {
+    topologicalSort({
+      size: 2,
+      adjacencyMatrix: {
+        a: { a: 0, b: 1 },
+        b: { a: 1, b: 0 },
+      },
+    });
+  });
+});
